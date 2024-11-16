d1f9ac84fc9a3abef7d8352b44c7cf1da59d4729
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-11-16 20:31:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Latiham Membuat Webapp Streamlit")
 
 st.write("Di malam Minggu belajar membuat webapp streamlit menghabiskan malam Minggu dengan produktif.")
+
+df = pd.read_csv("titanic.csv")
+st.write(df)
